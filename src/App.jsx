df59f5928dd705f7930b25bc2f06d49e93cd39c9
Thiago E Matumoto <thiagoeijimatumoto@gmail.com>
2024-01-23 20:04:51 -0300

    chore: 🔧 estruturação padrão do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -4,41 +4,15 @@ import DefaultButton from './components/default-button';
 import { primaryButtonTheme, secondaryButtonTheme } from './mui-theme/buttons';
 
 function App() {
-  const handlePrimaryButtonClick = () => {
-    console.log('Botão primário clicado!');
-  };
-
-  const handleSecondaryButtonClick = () => {
-    console.log('Botão secundário clicado!');
-  };
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <DefaultButton
         theme={primaryButtonTheme}
         label="Primary Button"
-        handleClick={handlePrimaryButtonClick}
       />
       <DefaultButton
         theme={secondaryButtonTheme}
         label="Secundary Button"
-        handleClick={handleSecondaryButtonClick}
       />
     </div>
   );
