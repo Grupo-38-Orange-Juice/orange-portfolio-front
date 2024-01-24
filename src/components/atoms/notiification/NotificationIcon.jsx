92727adfac080d649413306b90cbf9c0c95f256a
Thiago E Matumoto <thiagoeijimatumoto@gmail.com>
2024-01-24 20:22:44 -0300

    feat: :art: adding component with notification icon

---
+++ src/components/atoms/notiification/NotificationIcon.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import notificationIcon from '../../../assets/Icon-notifications.png';
+
+function NotificationIcon() {
+  return (
+    <div>
+      <img src={notificationIcon} alt="" />
+    </div>
+  );
+}
+
+export default NotificationIcon;
