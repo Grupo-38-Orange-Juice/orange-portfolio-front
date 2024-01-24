61f907ea5b89ab1f1ac43b7c94b433eb97d223c6
Thiago E Matumoto <thiagoeijimatumoto@gmail.com>
2024-01-24 20:22:02 -0300

    feat: :art: adding logo component

---
+++ src/components/atoms/logo/logo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import orangeJuice from '../../../assets/LOGO-ORANGEPORTFOLIO.png';
+
+function Logo() {
+  return (
+    <div>
+      <img src={orangeJuice} alt="" />
+    </div>
+  );
+}
+
+export default Logo;
