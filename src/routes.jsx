50ddbdf0c0b4b7e999c6fdf0808da3b1a386a9fd
Thiago E Matumoto <thiagoeijimatumoto@gmail.com>
2024-01-23 21:42:35 -0300

    refactor: :construction: adicionando estrutura das rotas

---
+++ src/routes.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import {
+  BrowserRouter, Routes, Route,
+} from 'react-router-dom';
+
+// function ProtectedRoutes(element) {
+//   return element;
+// }
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
