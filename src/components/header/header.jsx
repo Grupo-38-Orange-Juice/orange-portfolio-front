f8e8425ae9676ef65b40000a3b6f97874dd60cff
Thiago E Matumoto <thiagoeijimatumoto@gmail.com>
2024-01-24 20:59:11 -0300

    feat: :sparkles: adding header paragraph

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,12 +2,19 @@ import React from 'react';
 import Logo from './logo/logo';
 import Profile from '../profile/profile';
 import NotificationIcon from './notiification/NotificationIcon';
+import Paragraph from './paragraph/paragraph';
 
 function Header() {
   return (
     <header>
       <div>
         <Logo />
+        <Paragraph>
+          Meus projetos
+        </Paragraph>
+        <Paragraph>
+          Descobrir
+        </Paragraph>
       </div>
       <div>
         <Profile />
