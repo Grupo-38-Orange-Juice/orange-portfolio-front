040c898887c0c7886e770fb1f6db00bac97e99a9
Thiago E Matumoto <thiagoeijimatumoto@gmail.com>
2024-01-24 20:59:11 -0300

    feat: :sparkles: adding header paragraph

---
+++ src/components/header/paragraph/paragraph.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Paragraph({ children, change }) {
+  return (
+    <button type="button" onClick={change}>
+      {children}
+    </button>
+  );
+}
+
+Paragraph.propTypes = {
+  children: PropTypes.node.isRequired,
+  change: PropTypes.func.isRequired,
+};
+
+export default Paragraph;
