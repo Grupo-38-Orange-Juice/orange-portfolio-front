2f161d512019780278f3ae3901008229d9e924ba
Thiago E Matumoto <thiagoeijimatumoto@gmail.com>
2024-01-24 20:21:17 -0300

    feat: :sparkles: adding profile component

---
+++ src/components/atoms/profile/profile.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import profileImage from '../../../assets/profile.png';
+
+function Profile() {
+  return (
+    <div>
+      <img src={profileImage} alt="" />
+    </div>
+  );
+}
+
+export default Profile;
