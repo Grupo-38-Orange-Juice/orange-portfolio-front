147e0b5ad5d570fed3100d0ef566271fddc3de6b
cassius <cassiusbessa@gmail.com>
2024-01-24 18:29:00 -0300

    refactor: :art: change variable user to token and resolve cycle import dependencies

--- src/service/api.js
+++ src/service/api.js
@@ -1,20 +1,46 @@
-/* eslint-disable import/no-cycle */
 import axios from 'axios';
-import { getUserLocalStorage } from '../context/AuthProvider/util';
+import { getTokenStorage } from '../context/AuthProvider/util';
 
 const Api = axios.create({
   baseURL: 'https://url/',
 });
 
 Api.interceptors.request.use(
-
   (config) => {
-    const user = getUserLocalStorage();
-    // eslint-disable-next-line no-param-reassign
-    config.headers.Authorization = `Bearer ${user.token}`;
-    return config;
+    const token = getTokenStorage();
+    return {
+      ...config,
+      headers: {
+        ...config.headers,
+        Authorization: `Bearer ${token}`,
+      },
+    };
   },
   (error) => Promise.reject(error),
 );
+
+export async function loginResquest({ email, password }) {
+  try {
+    const request = await Api.post('authenticate', { email, password });
+    return request.data;
+  } catch (error) {
+    return error;
+  }
+}
+
+export async function createUser({
+  name, lastname, email, password,
+}) {
+  try {
+    const request = await Api.post('users', {
+      name, lastname, email, password,
+    })
+      .then((res) => ({ data: { ...res.data }, status: res.status }))
+      .catch((err) => err.response.data);
+    return request.data;
+  } catch (error) {
+    return error;
+  }
+}
 
 export default Api;
