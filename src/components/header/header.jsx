35439191433d8cd46549c61d84fa722cd45b7165
Thiago E Matumoto <thiagoeijimatumoto@gmail.com>
2024-01-24 21:07:33 -0300

    feat: :sparkles: adding navigation between pages in the header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,18 +1,29 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom'; // Importe useNavigate aqui
 import Logo from './logo/logo';
 import Profile from '../profile/profile';
 import NotificationIcon from './notiification/NotificationIcon';
 import Paragraph from './paragraph/paragraph';
 
 function Header() {
+  const navigate = useNavigate(); // Obtenha a função navigate usando useNavigate
+
+  function redirectToHome() {
+    navigate('/');
+  }
+
+  function redirectToExplore() {
+    navigate('/Explore');
+  }
+
   return (
     <header>
       <div>
         <Logo />
-        <Paragraph>
+        <Paragraph change={() => redirectToHome}>
           Meus projetos
         </Paragraph>
-        <Paragraph>
+        <Paragraph change={() => redirectToExplore}>
           Descobrir
         </Paragraph>
       </div>
