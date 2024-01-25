886e1ed24bbb1e599b234f96b2298e11a2a8399e
Thiago E Matumoto <thiagoeijimatumoto@gmail.com>
2024-01-24 22:38:58 -0300

    feat: :lipstick: adding style to the header

--- src/components/Header/Notiification/NotificationIcon.jsx
+++ src/components/Header/Notiification/NotificationIcon.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import notificationIcon from '../../../assets/Icon-notifications.png';
+import styles from './NotificationIcon.module.css';
 
 function NotificationIcon() {
   return (
-    <div>
-      <img src={notificationIcon} alt="" />
+    <div className={styles.notificationIcon}>
+      <img src={notificationIcon} alt="Icone de notificações" />
     </div>
   );
 }
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import profileImage from '../../assets/profile.png';
+import styles from './Profile.module.css';
 
 function Profile() {
   return (
-    <div>
+    <div className={styles.profileImage}>
       <img src={profileImage} alt="" />
     </div>
   );
