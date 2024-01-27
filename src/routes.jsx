9137edba257f521ef8609a395e69b4c288c1aad4
cassius <cassiusbessa@gmail.com>
2024-01-27 19:34:44 -0300

    feat: :sparkles: add home to routers

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/login';
 import Register from './pages/register';
+import Home from './pages/homePage/home';
 
 function Router() {
   return (
@@ -9,6 +10,7 @@ function Router() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
