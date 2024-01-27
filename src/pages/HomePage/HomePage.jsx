51b85570c011ccf39d27acaf60325a51b00c7244
Ellen <ellenc.contato@gmail.com>
2024-01-27 16:39:33 -0300

    feat: :sparkles: create CardPerfil component

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
+import CardPerfil from '../../components/CardPerfil';
 
 function HomePage() {
   return (
     <main>
       <Header />
+      <CardPerfil />
     </main>
   );
 }
