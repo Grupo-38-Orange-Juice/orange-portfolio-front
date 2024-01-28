c8261a1fa10001e6721ca1029a817955437b9233
CristianoRodr <cristrc@me.com>
2024-01-28 01:45:33 -0300

    feat: :memo: adding edit menu field
    adding edit menu field
    
    adding edit menu field

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './pages/login';
+import Login from './pages/Login/index';
 import Register from './pages/register';
 import HomePage from './pages/HomePage/HomePage';
+import Teste from './components/Menu/menu';
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/teste" element={<Teste />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/" element={<HomePage />} />
