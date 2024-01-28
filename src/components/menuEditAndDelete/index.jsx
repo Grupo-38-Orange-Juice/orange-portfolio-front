46137b7539c7107fba45dec8c7c694d18d803e0d
Ellen <ellenc.contato@gmail.com>
2024-01-27 21:17:12 -0300

    refactor: :construction: rebuilding menu edit component

--- src/components/menuEditAndDelete/index.jsx
+++ src/components/menuEditAndDelete/index.jsx
@@ -1,15 +1,45 @@
-import React, { useState, useRef, useEffect } from 'react';
-import Button from '@mui/material/Button';
-import ClickAwayListener from '@mui/material/ClickAwayListener';
-import Grow from '@mui/material/Grow';
-import Paper from '@mui/material/Paper';
-import Popper from '@mui/material/Popper';
-import MenuItem from '@mui/material/MenuItem';
-import MenuList from '@mui/material/MenuList';
-import Stack from '@mui/material/Stack';
+import React, { useState } from 'react';
+import { Menu, MenuItem } from '@mui/material';
+import iconEdit from '../../assets/icon_edit.svg';
 
 function MenuEditAndDelete() {
+  const [anchorElement, setAnchorElement] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorElement);
+
+  const handleClick = (event) => {
+    setAnchorElement(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorElement(null);
+  };
+
   return (
-    
+    <>
+      {/* aria-controls: se for true, abre edit-menu, se não, undefined */}
+      <img
+        id="iconEdit"
+        src={iconEdit}
+        alt="Menu Editar e Excluir"
+        onClick={handleClick}
+        aria-controls={open ? 'edit-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+      />
+      <Menu
+        id="edit-menu"
+        anchorEl={anchorElement}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'iconEdit',
+        }}
+      >
+        <MenuItem onClick={handleClose}>Editar</MenuItem>
+        <MenuItem onClick={handleClose}>Excluir</MenuItem>
+      </Menu>
+    </>
   );
 }
+
+export default MenuEditAndDelete;
