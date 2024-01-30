38973df35f20e2808e4a977e21629878babfd3e9
Ellen <ellenc.contato@gmail.com>
2024-01-30 01:47:30 -0300

    fix: :bug: add responsiveness

--- src/components/ProjContainer/ProjInfoFrame/index.jsx
+++ src/components/ProjContainer/ProjInfoFrame/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Box } from '@mui/material';
 import Profile from '../../Profile/Profile';
 import './style.css';
@@ -13,27 +13,48 @@ function getCurrentDate() {
 // estado para armazenar o nome do usuário
 function ProjInfoFrame() {
   const [user, setUser] = useState('');
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 600);
 
   // função para pegar nome do usuário
   const getUser = () => {
     setUser('Camila Soares');
   };
 
-  React.useEffect(() => {
+  useEffect(() => {
     getUser();
+
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 600);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
   }, []);
 
   const dateCreated = getCurrentDate();
 
   return (
     <Box className="proj_info_frame">
-      <Profile size="25" />
+      <Profile size="26" className="profile" />
       <p className="infos">
         {user}
-        {' '}
-        •
-        {' '}
-        {dateCreated}
+        {isMobile ? (
+          <>
+            <br />
+            {dateCreated}
+          </>
+        ) : (
+          /* se a tela for maior que 600px, rendeniza o "•" */
+          <>
+            {' '}
+            •
+            {' '}
+            {dateCreated}
+          </>
+        )}
       </p>
     </Box>
   );
