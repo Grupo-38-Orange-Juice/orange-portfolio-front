1ddd1b2fe26af3a9d87f56f81e6af212877fdf44
Ellen <ellenc.contato@gmail.com>
2024-01-30 05:36:25 -0300

    feat: :sparkles: create default container proj

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../../components/Header/Header';
 import CardPerfil from '../../components/CardPerfil';
 import ProjContainer from '../../components/ProjContainer';
+import DefaultContainer from '../../components/DefaultContainer';
 
 function HomePage() {
   return (
@@ -9,6 +10,7 @@ function HomePage() {
       <Header />
       <CardPerfil />
       <ProjContainer />
+      <DefaultContainer />
     </main>
   );
 }
