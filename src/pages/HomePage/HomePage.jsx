44525fcb002746bc1f126fffcfc21c934550ae35
Ellen <ellenc.contato@gmail.com>
2024-01-30 06:51:56 -0300

    fix: :construction: structuring home page
    implementar grid para galeria de projetos

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
+import { Box, Container, Grid } from '@mui/material';
+import TextField from '@mui/material/TextField';
 import Header from '../../components/Header/Header';
 import CardPerfil from '../../components/CardPerfil';
-import ProjContainer from '../../components/ProjContainer';
-import DefaultContainer from '../../components/DefaultContainer';
-import MenuEditAndDelete from '../../components/menuEditAndDelete';
 
 function HomePage() {
+  const projects = [{
+    id: 1, imgSrc: '', user: 'Camila Soares', location: 'Brasil',
+  }];
+
   return (
     <main>
       <Header />
-      <CardPerfil />
-      <ProjContainer />
-      <DefaultContainer />
-      <MenuEditAndDelete />
+      <Container>
+        <CardPerfil className="card_perfil" />
+        <Box className="box_proj" mt={3}>
+          <h6>Meus projetos</h6>
+          <TextField className="tag_text_field" id="outlined-basic" label="Buscar tags" sx={{ width: 513 }} />
+
+        </Box>
+      </Container>
     </main>
   );
 }
