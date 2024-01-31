f600107d2b5bd62547580a8346d70c589eedca1e
cassius <cassiusbessa@gmail.com>
2024-01-30 22:31:52 -0300

    Merge branch 'develop' into feature/add-menu-component

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login/Login';
 import Register from './pages/register';
 import HomePage from './pages/HomePage/HomePage';
+import Login from './pages/Login';
 
 function Router() {
   return (
