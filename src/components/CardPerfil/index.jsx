8fad159dadb6d7e277fb258d62c2cf8db667ce23
cassius <cassiusbessa@gmail.com>
2024-01-31 05:26:45 -0300

    feat: :sparkles: connect profile info to backend

--- src/components/CardPerfil/index.jsx
+++ src/components/CardPerfil/index.jsx
@@ -1,24 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Box } from '@mui/material';
 import PropTypes from 'prop-types';
 import Profile from '../Profile/Profile';
 import DefaultButton from '../default-button';
 import { primaryButtonTheme } from '../../mui-theme/buttons';
 import './style.css';
+import { AuthContext } from '../../context/AuthProvider/authProvider';
 
-function CardPerfil({
-  user = 'Camila Soares', location = 'Brasil',
-}) {
+function CardPerfil({ location = 'Brasil' }) {
+  const { user } = useContext(AuthContext);
+
   const handleAddProject = () => {
     window.location.href = '/adicionarprojeto';
     console.log('Projeto adicionado!');
   };
   return (
     <Box className="box__card_perfil">
-      <Profile className="profile" src="https://www.ecompletocdn.com.br/i/fp/1178/1521968_2_1692801033.jpg" size="130" />
+      <Profile className="profile" image={user ? user.image : 'https://www.ecompletocdn.com.br/i/fp/1178/1521968_2_1692801033.jpg'} size="130" />
       <Box className="container__right">
         <Box className="text_input">
-          <h5>{user}</h5>
+          <h5>{user ? user.fullName : 'Loading...'}</h5>
           <p className="location">{location}</p>
         </Box>
         <DefaultButton className="button_add" theme={primaryButtonTheme} label="Adicionar projeto" onClick={handleAddProject} size="sm" />
@@ -28,8 +29,11 @@ function CardPerfil({
 }
 
 CardPerfil.propTypes = {
-  user: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
+};
+
+PropTypes.defaultProps = {
+  location: 'Brasil',
 };
 
 export default CardPerfil;
