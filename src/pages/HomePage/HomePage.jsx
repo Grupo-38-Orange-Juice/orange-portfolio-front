f8091ff9818e2033457c2437b2218d94f7d6af64
Ellen <ellenc.contato@gmail.com>
2024-01-31 04:27:52 -0300

    refactor: :construction: building home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -6,18 +6,33 @@ import CardPerfil from '../../components/CardPerfil';
 
 function HomePage() {
   const projects = [{
-    id: 1, imgSrc: '', user: 'Camila Soares', location: 'Brasil',
+    id: 1,
+    imgSrc: '',
+    user: 'Camila Soares',
+    location: 'Brasil',
   }];
 
   return (
     <main>
       <Header />
       <Container>
-        <CardPerfil className="card_perfil" />
+        <div className="container_top">
+          <CardPerfil className="card_perfil" />
+        </div>
         <Box className="box_proj" mt={3}>
           <h6>Meus projetos</h6>
           <TextField className="tag_text_field" id="outlined-basic" label="Buscar tags" sx={{ width: 513 }} />
-
+          <Grid container spacing={3}>
+            {projects.length > 0 ? (
+              projects.map((project) => (
+                <Grid item key={project.id} xs={12} sm={6} md={4} lg={3}>
+                  <ProjContainer {...project} />
+                </Grid>
+              ))
+            ) : (
+              <DefaultContainer />
+            )}
+          </Grid>
         </Box>
       </Container>
     </main>
