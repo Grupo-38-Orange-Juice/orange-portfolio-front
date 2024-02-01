12e3b6778c6cb9b99792b60b8dd46edd66c38722
CristianoRodr <cristrc@me.com>
2024-02-01 19:55:35 -0300

    feat: :zap: Adjustment of warning messages

---
+++ src/pages/Teste/PageTesteModais.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Box } from '@mui/material';
+import ModalDelete from '../../components/ModalDelete/index';
+
+export default function TesteDeModais() {
+  return (
+    <Box>
+      <ModalDelete />
+    </Box>
+  );
+}
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Register from './pages/register';
+import TesteDeModais from './pages/Teste/PageTesteModais';
 import HomePage from './pages/HomePage/HomePage';
 import Login from './pages/Login';
 import AdicionarProjeto from './components/PortfolioRegistration/portfolioRegistration';
@@ -12,6 +13,7 @@ function Router() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/teste" element={<TesteDeModais />} />
         <Route
           path="/"
           element={(
