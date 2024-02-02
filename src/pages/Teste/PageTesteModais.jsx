9b57ce19551f7f9f34074231d8fbe9084758bf40
CristianoRodr <cristrc@me.com>
2024-02-02 16:04:48 -0300

    feat: :white_check_mark: adjusting routes for testing

--- src/pages/Teste/PageTesteModais.jsx
+++ src/pages/Teste/PageTesteModais.jsx
@@ -3,12 +3,13 @@ import { Box } from '@mui/material';
 // import ModalDelete from '../../components/ModalDelete';
 // import FeedbackDelete from '../../components/ModalFeedback';
 // import Concluded from '../../components/ModalConcluded/Index';
-import Added from '../../components/ModalAdd';
+// import Added from '../../components/ModalAdd';
+import ModalAddProject from '../../components/Modals/modalAddProject';
 
 export default function TesteDeModais() {
   return (
     <Box>
-      <Added />
+      <ModalAddProject />
     </Box>
   );
 }
