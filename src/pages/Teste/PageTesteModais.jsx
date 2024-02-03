6506090f4316109696647298217ec9546950784b
CristianoRodr <cristrc@me.com>
2024-02-03 10:05:47 -0300

    feat: :sparkles: Layout, font and responsiveness adjustment

--- src/pages/Teste/PageTesteModais.jsx
+++ src/pages/Teste/PageTesteModais.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Box } from '@mui/material';
-import ModalDelete from '../../components/ModalDelete';
-// import FeedbackDelete from '../../components/ModalFeedback';
+// import ModalDelete from '../../components/ModalDelete';
+import FeedbackDelete from '../../components/ModalFeedback';
 // import Concluded from '../../components/ModalConcluded/Index';
 // import Added from '../../components/ModalAdd';
 // import ModalAddProject from '../../components/Modals/modalAddProject';
@@ -9,7 +9,7 @@ import ModalDelete from '../../components/ModalDelete';
 export default function TesteDeModais() {
   return (
     <Box>
-      <ModalDelete />
+      <FeedbackDelete />
     </Box>
   );
 }
