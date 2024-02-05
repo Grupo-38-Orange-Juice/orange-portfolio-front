d719331623737a2e4e976b83344c57c7f6af0eae
cassius <cassiusbessa@gmail.com>
2024-02-05 05:28:15 -0300

    feat: :sparkles: add utils default props

--- src/pages/Explore/Explore.jsx
+++ src/pages/Explore/Explore.jsx
@@ -5,7 +5,7 @@ import { ThemeProvider, createTheme, responsiveFontSizes } from '@mui/material/s
 import Header from '../../components/Header/Header';
 import TextfieldResponsive from '../../components/TextfieldResponsive';
 import GridProjs from '../../components/GridProjs/index';
-import ModalViewSavedProj from '../../components/Modals/ModalViewSavedProj/modalViewSavedProj';
+// import ModalViewSavedProj from '../../components/Modals/ModalViewSavedProj/modalViewSavedProj';
 import { getProjects } from '../../service/orangeApi';
 
 const theme = createTheme();
@@ -137,7 +137,6 @@ function Explore() {
           <GridProjs projectsInfo={filteredProjects} toggleViewModal={toggleViewModal} />
         </Box>
       </Box>
-      <ModalViewSavedProj modalViewIsOpen={modalViewIsOpen} toggleViewModal={toggleViewModal} />
     </main>
   );
 }
